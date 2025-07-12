0f4aa8815adf9fe7eabb10116664a717b28d98ce
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2025-07-12 06:53:15 -0400

    Path fix

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import theme from './theme';
+import theme from './theme/theme';
 import { ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SnackbarProvider } from 'notistack';
