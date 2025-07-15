86d74dfa1c95b9ca3e1bc1ce61fe01697f5e8883
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2025-07-15 11:24:48 -0400

    Removing unused imports

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import theme from './theme/theme';
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SnackbarProvider } from 'notistack';
 import { BrowserRouter } from 'react-router-dom';
