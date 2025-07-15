abb0734bea2e838c0446b3d0fa8bd05e0b62365a
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2025-07-15 11:23:48 -0400

    Removes duplicate providers

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -8,14 +8,11 @@ import { BrowserRouter } from 'react-router-dom';
 const queryClient = new QueryClient();
 
 export const AppProviders: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <SnackbarProvider>
-            {children}
-          </SnackbarProvider>
-        </QueryClientProvider>
-      </BrowserRouter>
-  </ThemeProvider>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <SnackbarProvider>
+        {children}
+      </SnackbarProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
